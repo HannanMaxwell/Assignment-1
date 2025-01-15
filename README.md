1d9af0b45d768fee493252c5cd10dc233221d83e
HannanMaxwell <__max__@live.com>
2025-01-15 04:16:30 -0600

    Setup assignment 1 directory and files during initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Assignment 1
+
+## Description
+
+Assignment 1 will be used to teach us how to setup a repository on GitHub, how to use a text editor such as VSCode to write code in Python, and using Git to push changes to the remote remote repository on GitHub.
+
+## Author
+
+Muhammad Rahmani
---
+++ hello.py
@@ -0,0 +1,10 @@
+"""This module prints hello world, and is used to demonstrate how to use git"""
+
+__author__ = "Muhammad Rahmani "
+__version__ = "1.0.0"
+
+# Assigning "Hello world!" string to the variable hello_world.
+hello_world = "Hello world!"
+
+# Prints the variable hello_world
+print(f"{hello_world}")
